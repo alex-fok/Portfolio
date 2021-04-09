89cdd11e53fc8efc488e238be28b4254f1a1642f
Alexander Fok <foktmalexander@gmail.com>
2021-04-08 22:16:12 -0700

    Add 'as' attribute to immitate 'Link' behavior

--- src/components/Navigator/index.js
+++ src/components/Navigator/index.js
@@ -1,17 +1,18 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigator = () => {
+  
   return (
     <Navbar bg='dark' variant='dark'>
       <Navbar.Brand>Alex Fok</Navbar.Brand>
         <Nav className='mr-auto'>
         <Nav.Item>
-          <Nav.Link><Link to='/'>About Me</Link></Nav.Link>
+          <Nav.Link as={Link} to='/'>About Me</Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link><Link to='/more'>More</Link></Nav.Link>
+        <Nav.Item to='/more'>
+          <Nav.Link as={Link} to='/more'>More</Nav.Link>
         </Nav.Item>
         </Nav>
     </Navbar>
